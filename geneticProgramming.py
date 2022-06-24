9d27ce4ea4891e602828005c3ca3dbc9b93b644f
DeaconSeals <deaconseals18@gmail.com>
2022-06-23 20:27:01 -0500

    Migrate to public repos

---
+++ geneticProgramming.py
@@ -0,0 +1,14 @@
+from baseEvolution import baseEvolutionPopulation
+
+class geneticProgrammingPopulating(baseEvolutionPopulation):
+	def generate_children(self):
+		children = list()
+		# TODO: Select parents
+		# hint: self.parent_selection(self.population, **self.parent_selection_kwargs)
+		pass
+
+		# TODO: Generate children by either recombining two parents OR
+		#		generating a mutated copy of a single parent
+		pass
+
+		return children
